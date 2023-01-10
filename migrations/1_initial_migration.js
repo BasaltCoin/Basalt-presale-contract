68255594da20016400cead2c7d3160be275079d2
darsioNet <mobilhex@gmail.com>
2023-01-10 23:19:31 +0200

    bsctest deploy script

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -3,12 +3,25 @@ const BasaltToken = artifacts.require("BasaltToken");
 
 module.exports = async (deployer, network) => {
   if (network == "bscmain") {
+    const BUSD_ADDRESS="0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
     try {
       const paymentToken={
         tokenAddress:BUSD_ADDRESS,
         price:web3.utils.toWei("2", "ether"),
     }
       await deployer.deploy(BasaltToken);
+      await deployer.deploy(BasaltTokenSale,BasaltToken.address,paymentToken);
+    } catch (err) {
+      console.log("ERROR:", err);
+    }
+  }else if(network == "bsctest") {
+    try {
+      const BUSD_ADDRESS="0x90a2182EBb0F5D4E171CbD0C22fb8682ec923cd4";
+      const paymentToken={
+        tokenAddress:BUSD_ADDRESS,
+        price:web3.utils.toWei("1", "ether"),
+      }
+      await deployer.deploy(BasaltToken,web3.utils.toWei("1000000", "ether"));
       await deployer.deploy(BasaltTokenSale,BasaltToken.address,paymentToken);
     } catch (err) {
       console.log("ERROR:", err);
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 truffle migrate --reset --network $1
 echo "please wait...30 sec"
 sleep 30
+#truffle run verify MockERC20 --network $1
 truffle run verify BasaltToken --network $1
 truffle run verify BasaltTokenSale --network $1
 
