86f51b8ad1ff4591867c2aa8f6d1e1bdb66e15f4
darsioNet <mobilhex@gmail.com>
2023-01-10 23:29:50 +0200

    fix deploy script

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -4,12 +4,13 @@ const BasaltToken = artifacts.require("BasaltToken");
 module.exports = async (deployer, network) => {
   if (network == "bscmain") {
     const BUSD_ADDRESS="0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
+    const PRE_MINT_AMOUNT=web3.utils.toWei("1000000", "ether");
     try {
       const paymentToken={
         tokenAddress:BUSD_ADDRESS,
         price:web3.utils.toWei("2", "ether"),
     }
-      await deployer.deploy(BasaltToken);
+      await deployer.deploy(BasaltToken,PRE_MINT_AMOUNT);
       await deployer.deploy(BasaltTokenSale,BasaltToken.address,paymentToken);
     } catch (err) {
       console.log("ERROR:", err);
@@ -17,11 +18,12 @@ module.exports = async (deployer, network) => {
   }else if(network == "bsctest") {
     try {
       const BUSD_ADDRESS="0x90a2182EBb0F5D4E171CbD0C22fb8682ec923cd4";
+      const PRE_MINT_AMOUNT=web3.utils.toWei("1000000", "ether");
       const paymentToken={
         tokenAddress:BUSD_ADDRESS,
         price:web3.utils.toWei("1", "ether"),
       }
-      await deployer.deploy(BasaltToken,web3.utils.toWei("1000000", "ether"));
+      await deployer.deploy(BasaltToken,PRE_MINT_AMOUNT);
       await deployer.deploy(BasaltTokenSale,BasaltToken.address,paymentToken);
     } catch (err) {
       console.log("ERROR:", err);
